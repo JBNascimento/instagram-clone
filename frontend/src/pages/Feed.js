4458fef8bd72a58ef12700bc7228741bb4d0836a
JefBNascimento <jbnascimento@msn.com>
2020-03-06 20:05:55 -0300

    Add feed style

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,9 +1,73 @@
 import React, { Component } from "react";
 
+import './Feed.css';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
 class Feed extends Component{
     render(){
         return(
-            <h1> Feed</h1>
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Jeferson Nascimento</span>
+                            <span className="place"> Rio de Janeiro</span>
+                        </div>
+
+
+                        <img src={more} alt="Mais" />    
+                    </header>
+
+                    <img src="http://localhost:3333/files/avatarTonho.jpg" alt="" />
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="" />
+                            <img src={comment} alt="" />
+                            <img src={send} alt="" />    
+                        </div>
+                        <strong> 900 curtidas </strong>
+                        <p> Um post maneirão!
+                            <span> #react #react-native #node</span>
+                        </p>
+
+                    </footer>
+
+                </article>
+
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Jeferson Nascimento</span>
+                            <span className="place"> Rio de Janeiro</span>
+                        </div>
+
+
+                        <img src={more} alt="Mais" />    
+                    </header>
+
+                    <img src="http://localhost:3333/files/avatarTonho.jpg" alt="" />
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="" />
+                            <img src={comment} alt="" />
+                            <img src={send} alt="" />    
+                        </div>
+                        <strong> 900 curtidas </strong>
+                        <p> Um post maneirão!
+                            <span> #react #react-native #node</span>
+                        </p>
+
+                    </footer>
+
+                </article>
+                
+            </section>            
         );
     }
 }
