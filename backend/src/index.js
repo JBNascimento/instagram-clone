7d3f9a0388ff8f40efeb161d73839d32d98f3051
JefBNascimento <jbnascimento@msn.com>
2020-03-06 15:37:32 -0300

    Add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://dbuser:dbuser123@cluster0-8pofn.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser:true,
 })
+
+app.use(cors());
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
