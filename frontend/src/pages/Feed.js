8e10e4c2693e0d8db139cc8555554309382ac1c8
JefBNascimento <jbnascimento@msn.com>
2020-03-06 20:22:45 -0300

    Feed - API access

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -9,64 +9,51 @@ import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 class Feed extends Component{
+    state = {
+        feed:[],
+    };
+
+    async componentDidMount() {
+        const response = await api.get('posts');
+        this.setState({ feed: response.data });
+    }
+
+
+
     render(){
         return(
             <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Jeferson Nascimento</span>
-                            <span className="place"> Rio de Janeiro</span>
-                        </div>
+                { this.state.feed.map(post => (
+                                    <article key={post._id}>
+                                    <header>
+                                        <div className="user-info">
+                                            <span>{post.author}</span>
+                                            <span className="place"> {post.place}</span>
+                                        </div>
+                
+                
+                                        <img src={more} alt="Mais" />    
+                                    </header>
+                
+                                    <img src={`http://localhost:3333/files/${post.image}`} alt="" />
+                
+                                    <footer>
+                                        <div className="actions">
+                                            <img src={like} alt="" />
+                                            <img src={comment} alt="" />
+                                            <img src={send} alt="" />    
+                                        </div>
+                                        <strong> {post.likes} curtidas </strong>
+                                        <p> {post.description}
+                                            <span> {post.hashtags}</span>
+                                        </p>
+                
+                                    </footer>
+                
+                                </article>
+                ))}
 
-
-                        <img src={more} alt="Mais" />    
-                    </header>
-
-                    <img src="http://localhost:3333/files/avatarTonho.jpg" alt="" />
-
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="" />
-                            <img src={comment} alt="" />
-                            <img src={send} alt="" />    
-                        </div>
-                        <strong> 900 curtidas </strong>
-                        <p> Um post maneirão!
-                            <span> #react #react-native #node</span>
-                        </p>
-
-                    </footer>
-
-                </article>
-
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Jeferson Nascimento</span>
-                            <span className="place"> Rio de Janeiro</span>
-                        </div>
-
-
-                        <img src={more} alt="Mais" />    
-                    </header>
-
-                    <img src="http://localhost:3333/files/avatarTonho.jpg" alt="" />
-
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="" />
-                            <img src={comment} alt="" />
-                            <img src={send} alt="" />    
-                        </div>
-                        <strong> 900 curtidas </strong>
-                        <p> Um post maneirão!
-                            <span> #react #react-native #node</span>
-                        </p>
-
-                    </footer>
-
-                </article>
+              
 
             </section>            
         );
